c8cfe443d2dc230c9ec909f6bd18cafb002fe8bf
davine94 <davine94>
2022-03-14 00:19:09 +0300

    Includes the mouseover function on the portfolio

--- js/app.js
+++ js/app.js
@@ -34,3 +34,66 @@ $(document).ready(function(){
     $("#pImage").slideDown();
   });
 });
+
+$(document).ready(function(){
+    $("#work-1").mouseover(function(){
+      $("#layover").show();
+    }).mouseout(function(){
+      $("#layover").hide();
+    });
+  });
+
+$(document).ready(function(){
+$("#work-2").mouseover(function(){
+$("#layover1").show();
+}).mouseout(function(){
+$("#layover1").hide();
+});
+});
+
+$(document).ready(function(){
+    $("#work-3").mouseover(function(){
+      $("#layover2").show();
+    }).mouseout(function(){
+      $("#layover2").hide();
+    });
+  });
+
+$(document).ready(function(){
+$("#work-4").mouseover(function(){
+$("#layover3").show();
+}).mouseout(function(){
+$("#layover3").hide();
+});
+});
+
+$(document).ready(function(){
+  $("#work-5").mouseover(function(){
+  $("#layover4").show();
+  }).mouseout(function(){
+  $("#layover4").hide();
+  });
+  });
+
+$(document).ready(function(){
+    $("#work-6").mouseover(function(){
+    $("#layover5").show();
+    }).mouseout(function(){
+    $("#layover5").hide();
+    });
+    });
+  
+$(document).ready(function(){
+      $("#work-7").mouseover(function(){
+      $("#layover6").show();
+      }).mouseout(function(){
+      $("#layover6").hide();
+      });
+      });
+$(document).ready(function(){
+        $("#work-8").mouseover(function(){
+        $("#layover7").show();
+        }).mouseout(function(){
+        $("#layover7").hide();
+        });
+        });
